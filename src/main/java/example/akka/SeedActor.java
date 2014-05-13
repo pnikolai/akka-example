1fe0a407bc97b31dde4b5f8ae685c5ce5abf9567
Svilen Ivanov <isvilen@applicata.bg>
2014-05-13 15:09:07 +0300

    Refactor SeedActor.

--- src/main/java/example/akka/SeedActor.java
+++ src/main/java/example/akka/SeedActor.java
@@ -3,6 +3,7 @@ package example.akka;
 import akka.actor.UntypedActor;
 import akka.cluster.Cluster;
 import akka.cluster.ClusterEvent;
+import akka.cluster.Member;
 import akka.cluster.ClusterEvent.MemberEvent;
 import akka.cluster.ClusterEvent.MemberUp;
 import akka.cluster.ClusterEvent.MemberRemoved;
@@ -30,19 +31,31 @@ public class SeedActor extends UntypedActor {
   public void onReceive(Object message) {
     if (message instanceof MemberUp) {
       MemberUp mUp = (MemberUp) message;
-      log.info("Member is Up: {} {}", mUp.member(), mUp.member().getRoles());
+      onMemberUp(mUp.member());
 
     } else if (message instanceof UnreachableMember) {
       UnreachableMember mUnreachable = (UnreachableMember) message;
-      log.info("Member detected as unreachable: {}", mUnreachable.member());
+      onMemberUnreachable(mUnreachable.member());
 
     } else if (message instanceof MemberRemoved) {
       MemberRemoved mRemoved = (MemberRemoved) message;
-      log.info("Member is Removed: {}", mRemoved.member());
+      onMemberRemoved(mRemoved.member());
 
     } else if (message instanceof MemberEvent) {
       // ignore
     } else
       unhandled(message);
+  }
+
+  private void onMemberUp(Member member) {
+    log.info("Member is Up: {} {}", member, member.getRoles());
+  }
+
+  private void onMemberUnreachable(Member member) {
+    log.info("Member detected as unreachable: {}", member);
+  }
+
+  private void onMemberRemoved(Member member) {
+    log.info("Member is Removed: {}", member);
   }
 }
